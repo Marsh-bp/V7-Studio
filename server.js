7a605bacafdca0a9bf331880596c0d349542cd06
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-06-25 19:56:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'V7_studio.html'));
+});
+
+app.post('/save-phone-number', (req, res) => {
+    console.log('Received request to save phone number');
+    const name = req.body.name;
+    const phoneNumber = req.body.phoneNumber;
+    const data = `${name} - ${phoneNumber}\n`;
+    fs.appendFile('phone-numbers.txt', data, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+            res.status(500).send({ message: 'Failed to save phone number' });
+        } else {
+            console.log('Name and phone number saved successfully');
+            res.status(200).send({ message: 'Name and phone number saved successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
