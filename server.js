8b5dcbbcbe179bb5183ec344c7365fde5bf4d6f0
Marsh-bp <128377428+Marsh-bp@users.noreply.github.com>
2024-06-25 20:27:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+
+// Use the PORT environment variable if available, otherwise default to 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
